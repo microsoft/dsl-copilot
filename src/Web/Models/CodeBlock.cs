dc91b62abc5c4701dd9a5d1880756cd489115e7a
Pete Messina <petermessina@microsoft.com>
2024-05-12 19:57:02 -0400

    Fixing prompt structure

---
+++ src/Web/Models/CodeBlock.cs
@@ -0,0 +1,11 @@
+﻿namespace DslCopilot.Web.Models
+{
+  public record CodeBlock
+  {
+    public string Prompt { get; set; }
+
+    public string AdditionalDetails { get; set; }
+
+    public string Response { get; set; }
+  }
+}
---
+++ src/Web/Models/LanguageExamples.cs
@@ -0,0 +1,9 @@
+﻿namespace DslCopilot.Web.Models
+{
+  public record LanguageExamples
+  {
+    public string Language { get; set; }
+
+    public List<CodeBlock> Prompts { get; set; }
+  }
+}
